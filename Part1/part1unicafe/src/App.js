6c717c6175dc7c1d55d1a6797452f3245c3bf78c
lassib <91030337+lassib@users.noreply.github.com>
2022-11-14 11:31:10 +0200

    1.6 actually done

--- Part1/part1unicafe/src/App.js
+++ Part1/part1unicafe/src/App.js
@@ -1,14 +1,51 @@
 import { useState } from 'react'
 
+const Title = (props) => (
+  <div>
+    <h1>{props.title}</h1>
+  </div>
+)
+
+const Statistics = (props) => {
+  return(
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td>{props.good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td>{props.neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td>{props.bad}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>;
+
 const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const feedback = "give feedback";
+  const statistics = "statisctics";
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <Title title={feedback} />
+      <Button handleClick={() => setGood(good +1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Title title={statistics} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
