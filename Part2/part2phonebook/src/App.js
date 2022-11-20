7768cad3e5edb64dd76594bcb70542f4ba8db391
lassib <91030337+lassib@users.noreply.github.com>
2022-11-20 18:42:46 +0200

    2.10 done

--- Part2/part2phonebook/src/App.js
+++ Part2/part2phonebook/src/App.js
@@ -1,5 +1,80 @@
 import { useState } from "react";
 
+const Filter = (props) => {
+  const handleFilterChange = (event) => {
+    props.setFilter(event.target.value);
+    props.setFilterPersons(
+      props.persons.filter((person) =>
+        person.name
+          .toLocaleLowerCase()
+          .includes(event.target.value.toLocaleLowerCase())
+      )
+    );
+  };
+
+  return (
+    <div>
+      filter shown with <input onChange={handleFilterChange} />
+    </div>
+  );
+};
+
+const PersonForm = (props) => {
+  const handleNewPerson = (event) => {
+    event.preventDefault();
+    const nameArray = props.persons.map((obj) => obj.name);
+    const addPerson = [
+      {
+        name: props.newName,
+        number: props.newNumber,
+      },
+    ];
+
+    if (nameArray.includes(`${props.newName}`)) {
+      alert(`${props.newName} is already added to phonebook`);
+    } else {
+      props.setPersons(props.persons.concat(addPerson));
+    }
+  };
+
+  const handleNameChange = (event) => {
+    props.setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    props.setNewNumber(event.target.value);
+  };
+
+  return (
+    <form>
+      <div>
+        name: <input onChange={handleNameChange} />
+        <br></br>
+        number: <input onChange={handleNumberChange} />
+        <br></br>
+        <button type="submit" onClick={handleNewPerson}>
+          add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+const Persons = (props) => {
+  let shownPersons = "";
+  props.filter === "" ? (shownPersons = props.persons) : (shownPersons = props.filterPersons);
+
+  return (
+    <ul>
+      {shownPersons.map((person, i) => (
+        <li key={i}>
+          {person.name} {person.number}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: "Arto Hellas", number: "040-123456", id: 1 },
@@ -12,86 +87,31 @@ const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
-  const Persons = (props) => {
-    let shownPersons = "";
-    filter === ""
-      ? (shownPersons = props.persons)
-      : (shownPersons = props.filterPersons);
-
-    return (
-      <ul>
-        {shownPersons.map((person, i) => (
-          <li key={i}>
-            {person.name} {person.number}
-          </li>
-        ))}
-      </ul>
-    );
-  };
-
-  const handleNewPerson = (event) => {
-    event.preventDefault();
-    const nameArray = persons.map((obj) => obj.name);
-    const addPerson = [
-      {
-        name: newName,
-        number: newNumber,
-      },
-    ];
-
-    if (nameArray.includes(`${newName}`)) {
-      alert(`${newName} is already added to phonebook`);
-    } else {
-      setPersons(persons.concat(addPerson));
-    }
-  };
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-    setFilterPersons(
-      persons.filter((person) =>
-        person.name
-          .toLocaleLowerCase()
-          .includes(event.target.value.toLocaleLowerCase())
-      )
-    );
-  };
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <div>
-          filter shown with <input onChange={handleFilterChange} />
-        </div>
-        <div>
-          <h2>add a new</h2>
-          name: <input onChange={handleNameChange} />
-          <br></br>
-          number: <input onChange={handleNumberChange} />
-          <br></br>
-          <button type="submit" onClick={handleNewPerson}>
-            add
-          </button>
-        </div>
-      </form>
+      <Filter
+        persons={persons}
+        setFilterPersons={setFilterPersons}
+        setFilter={setFilter}
+      />
+      <h3>Add a new</h3>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+      />
       <div>
-        <h2>Numbers</h2>
+        <h3>Numbers</h3>
         <Persons
           persons={persons}
           filterPersons={filterPersons}
           filter={filter}
-        ></Persons>
+        />
       </div>
-      <div>debug: {newName}</div>
     </div>
   );
 };
