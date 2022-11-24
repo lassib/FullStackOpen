d3bc07e6e5c35d65a5d9921195367fe837a19c28
lassib <91030337+lassib@users.noreply.github.com>
2022-11-24 17:51:32 +0200

    2.12 done

---
+++ Part2/part2restcountries/src/App.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const Country = (props) => {
+  return(
+    <div>
+      <h1>{props.country.name.common}</h1>
+        <p>capital: {props.country.capital}</p>
+        <p>area: {props.country.area}</p>
+        <h4>Languages:</h4>
+        <ul>
+          {Object.values(props.country.languages).map((language) => (
+            <li key={language}>{language}</li>
+          ))}
+        </ul>
+        <br />
+        <img src={props.country.flags.svg} height={150} alt="Flag was not found"></img>
+    </div>
+  )
+}
+
+const CountryFilter = (props) => {
+  const countryData = props.countries.filter((country) =>
+    country.name.common
+      .toLowerCase()
+      .includes(props.filterCountry.toLowerCase())
+  );
+
+  if (countryData.length > 9) return <p>Too many matches, specify another filter</p>;
+
+  else if (countryData.length < 2){
+    return (
+      <div>
+        {countryData.map((country => (
+          <Country key={country.name.common} country={country}/>
+        )))}
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <div>
+        {countryData.map((country) => (
+          <p key={country.name.common}>{country.name.common}</p>
+        ))}
+      </div>
+    );
+  }
+};
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [filterCountry, setFilterCountry] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setFilterCountry(event.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <div>
+          find countries <input onChange={handleCountryChange} />
+        </div>
+      </form>
+      <CountryFilter filterCountry={filterCountry} countries={countries} />
+    </div>
+  );
+}
+
+export default App;
