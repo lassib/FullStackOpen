858b009d414485a393998c718fba6ac7e6e708b3
lassib <91030337+lassib@users.noreply.github.com>
2022-11-24 16:54:58 +0200

    2.11 done

--- Part2/part2phonebook/src/App.js
+++ Part2/part2phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Filter = (props) => {
   const handleFilterChange = (event) => {
@@ -62,7 +63,9 @@ const PersonForm = (props) => {
 
 const Persons = (props) => {
   let shownPersons = "";
-  props.filter === "" ? (shownPersons = props.persons) : (shownPersons = props.filterPersons);
+  props.filter === ""
+    ? (shownPersons = props.persons)
+    : (shownPersons = props.filterPersons);
 
   return (
     <ul>
@@ -76,16 +79,17 @@ const Persons = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [filterPersons, setFilterPersons] = useState(persons);
   const [filter, setFilter] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/persons`).then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   return (
     <div>
